5aea38ae2a404d0aed2868970c7f33ad6c492a11
Daniell <i513275@fhict.nl>
2023-11-21 14:53:40 +0100

    map changes

--- kaart_script.js
+++ kaart_script.js
@@ -1,13 +1,18 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-map.locate({setView: true, maxZoom: 16});
+var map = L.map('map').fitWorld();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
     attribution: 'CrimeQuest'
 }).addTo(map);
 
+// =============================== Map load================================== //
+
+map.locate({setView: true, maxZoom: 16});
+
+// =============================== Laat je eigen locatie zien =========================== //
+
 function onLocationFound(e) {
-    var radius = e.accuracy;
+    var radius = 300;
 
     L.marker(e.latlng).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
@@ -17,17 +22,12 @@ function onLocationFound(e) {
 
 map.on('locationfound', onLocationFound);
 
+// =============================== Laat je locatie nauwkeurig zien=========================== //
+
 function onLocationError(e) {
     alert(e.message);
 }
 
 map.on('locationerror', onLocationError);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-
+// =============================== Laat een error zien als hij je locatie niet kan vinden ================================ //
