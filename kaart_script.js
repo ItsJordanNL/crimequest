d23a8525df94bfde8b591c188b6168bec2269624
Daniell <i513275@fhict.nl>
2023-11-21 16:32:29 +0100

    kaart page v0.1

--- kaart_script.js
+++ kaart_script.js
@@ -1,18 +1,21 @@
-var map = L.map('map').fitWorld();
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: 'CrimeQuest'
+var map = L.map('map').setView([51.44083, 5.47778], 16);
+// var map = L.map('map').fitWorld();
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: 'CrimeQuest',
 }).addTo(map);
+
+
 
 // =============================== Map load================================== //
 
-map.locate({setView: true, maxZoom: 16});
+// map.locate({setView: true, maxZoom: 16});
 
 // =============================== Laat je eigen locatie zien =========================== //
 
 function onLocationFound(e) {
-    var radius = 300;
+    var radius = 100;
 
     L.marker(e.latlng).addTo(map)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
@@ -32,4 +35,23 @@ map.on('locationerror', onLocationError);
 
 // =============================== Laat een error zien als hij je locatie niet kan vinden ================================ //
 
-L.marker([51.505, -0.09]).addTo(map);
+L.marker([51.44083, 5.47778]).addTo(map);
+L.marker([51.44371, 5.47778]).addTo(map);
+L.marker([51.4415, 5.4828]).addTo(map);
+
+
+L.circle([51.44083, 5.47778],1000).addTo(map);
+
+
+// =============================== Markers & circles ============================ //
+
+L.control.coordinates({
+    position:"topright",
+    useDMS: false,
+    labelTemplateLat: "N {y}",
+    labelTemplateLatLng: "E {x}",
+    useLatLngOrder: true,
+}).addTo(map)
+
+// =============================== Zoek coordinaten functie=========================== //
+
