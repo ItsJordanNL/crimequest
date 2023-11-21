1ab92379fee854c91254529c0fbfbaaee8fdbc29
Daniell <i513275@fhict.nl>
2023-11-21 14:42:23 +0100

    map import test

---
+++ kaart_script.js
@@ -0,0 +1,33 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+map.locate({setView: true, maxZoom: 16});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'CrimeQuest'
+}).addTo(map);
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
