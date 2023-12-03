642adf878ba7a92a931acad705bfc99c4ef0dcc7
Daan van der Vorst <i520285@fhict.nl>
2023-12-03 14:22:46 +0100

    pagina werken nu

--- instructie.js
+++ instructie.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Toon de eerste popup bij het laden van de pagina
     document.getElementById('popup1').style.display = 'flex';
+    document.querySelector('.indicator-dot').classList.add('active');
 });
 
 function changePopup(popupNumber) {
@@ -11,4 +12,12 @@ function changePopup(popupNumber) {
 
     // Toon de geselecteerde popup
     document.getElementById('popup' + popupNumber).style.display = 'flex';
+
+    // Verwijder de active class van alle indicator dots
+    document.querySelectorAll('.indicator-dot').forEach(dot => {
+        dot.classList.remove('active');
+    });
+
+    // Voeg de active class toe aan de geselecteerde indicator dot
+    document.querySelector('.indicator-dot:nth-child(' + popupNumber + ')').classList.add('active');
 }
