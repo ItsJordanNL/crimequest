76295acc6bbf2f6975f598fa69339889b594321b
Jordan <j.koevoets@student.fontys.nl>
2023-12-04 12:52:35 +0100

    Aanpassingen Semmy dader pagina

--- js/dader.js
+++ js/dader.js
@@ -4,7 +4,6 @@ const box2 = document.querySelector('.box2');
 const bulletsContainer = document.getElementById('bullets');
 
 let startX;
-let initialTransform = 0;
 let currentIndex = 0;
 
 container.addEventListener('touchstart', handleTouchStart, { passive: false });
@@ -13,44 +12,33 @@ container.addEventListener('touchend', handleTouchEnd, { passive: false });
 
 function handleTouchStart(e) {
     startX = e.touches[0].clientX;
-    initialTransform = getCurrentTransform();
 }
 
 function handleTouchMove(e) {
     const currentX = e.touches[0].clientX;
     const diff = startX - currentX;
 
-    setTransform(initialTransform - diff);
+    setTransform(diff);
 }
 
 function handleTouchEnd() {
     const diff = startX - event.changedTouches[0].clientX;
 
     if (Math.abs(diff) > container.clientWidth / 4) {
-        currentIndex = (currentIndex + 1) % 2;
+        // Toggle the order of the boxes
+        currentIndex = 1 - currentIndex;
     }
 
     // Reset
     setTransform(0);
 
-    // posities van de boxen worden gespiegeld
-    [box.style.order, box2.style.order] = [box2.style.order, box.style.order];
-
     updateBullets();
-}
-
-function getCurrentTransform() {
-    const transformValue = window.getComputedStyle(box).getPropertyValue('transform');
-    const matrix = new WebKitCSSMatrix(transformValue);
-    return matrix.m41;
 }
 
 function setTransform(percentage) {
     box.style.zIndex = currentIndex === 0 ? '2' : '1';
-    box.style.left = currentIndex === 0 ? '0' : '14px';
-    box2.style.left = currentIndex === 0 ? '12px' : '-5px';
-    box.style.transform = `translateX(${percentage}%)`;
-    box2.style.transform = `translateX(${percentage + 1}%)`;
+    box.style.left = currentIndex === 0 ? `${percentage}px` : `${14 + percentage}px`;
+    box2.style.left = currentIndex === 0 ? `${14 + percentage}px` : `${percentage}px`;
 }
 
 function updateBullets() {
